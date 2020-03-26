1c20cab06ff9fcb59adc955a693b30c48a144f9a
Florian Klockenkemper <fk@shopware.com>
2020-03-26 14:44:18 +0100

    add login state and handling

--- src/MerchantFrontend/src/app/core/core.module.ts
+++ src/MerchantFrontend/src/app/core/core.module.ts
@@ -1,16 +1,29 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import {CommonModule, Location } from '@angular/common';
 import {LayoutsModule} from './layouts/layouts.module';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginService } from './services/login.service';
+import { onAppInit } from './app-init';
+import { LocalStorageService } from './services/local-storage.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     LayoutsModule
   ],
   declarations: [],
   exports: [
     LayoutsModule
   ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: onAppInit,
+      multi: true,
+      deps: [Location, LoginService, LocalStorageService]
+    }
+  ]
 })
 export class CoreModule {
 }
