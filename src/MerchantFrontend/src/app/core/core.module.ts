fc3dabf2d64e5bbf21c7457bd14e855988ac2824
Arnold Stoba <me@arnoldstoba.com>
2020-03-26 19:31:37 +0100

    Add basic button and font styles

--- src/MerchantFrontend/src/app/core/core.module.ts
+++ src/MerchantFrontend/src/app/core/core.module.ts
@@ -1,16 +1,21 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LayoutsModule} from './layouts/layouts.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutsModule } from './layouts/layouts.module';
+import { MerchantApiService } from './services/merchant-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    LayoutsModule
+    LayoutsModule,
+    HttpClientModule
   ],
   declarations: [],
   exports: [
     LayoutsModule
   ],
+  providers: [
+    MerchantApiService
+  ]
 })
-export class CoreModule {
-}
+export class CoreModule {}
--- src/MerchantFrontend/src/app/core/models/authority.model.ts
+++ src/MerchantFrontend/src/app/core/models/authority.model.ts
@@ -1,5 +1,5 @@
 export interface Authority {
-    id: string;
-    name: string;
-    domain: string;
+  id: number;
+  name: string;
+  domain: string;
 }
